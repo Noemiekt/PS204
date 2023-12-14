e535542d6f933b3e18818ee9de4403294febeaf9
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2023-12-14 11:30:08 +0100

    classifier alex

---
+++ classifieur.m
@@ -0,0 +1,36 @@
+function [phi] = classifieur(x,S,Bx,k,m)
+% x = image en entrée
+% S = sous-espace vectoriel S engendré par les l eigenfaces les plus énergétiques
+% Bx = base d’apprentissage
+% k = nombre de plus proche voisin
+% m = le nombre d'individu
+
+x_moy = mean(x); %Moyenne empirique de x
+
+[~,l]=size(S); % l = nb eigenfaces les plus énergétiques (facespace).
+
+w = (x-x_moy).' * S;
+
+[~,Ni]=size(Bx); % Ni = nombres d'images
+
+wbase=zeros(l,Ni); % Stocker les w des images de la bases d'apprentissages.
+
+for i=1:Ni
+    xi_moy=mean(Bx(:,i));
+    wbase(:,i)=(Bx(:,i)-xi_moy).' * S;
+end
+
+distances = sqrt(sum((wbase - repmat(w, 1, Ni)).^2)); % Calcul des distances entre w de l'image et w de la base
+
+% Tri des distances et obtention des indices des k plus proches voisins
+[~, indices] = sort(distances);
+
+% Sélection des k plus proches voisins
+k_plus_proches = indices(1:k);
+
+classes = floor(k_plus_proches/m);
+
+phi=mode(classes); %classifieur k-NN
+
+end
+
